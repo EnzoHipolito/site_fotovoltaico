ff8a064a40e15ad616e7bcd4967e0664db334a92
ENZO DOS SANTOS HIPÓLITO <enzo_hipolito@estudante.sesisenai.org.br>
2025-10-08 15:52:49 -0300

    adição de imagens/videos + pesquisa + calculadora

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+let resultado = document.getElementById("resultado")
+
 function navegar(pagina) {
-    document.querySelectorAll("section").forEach(sec => sec.classList.remove("active"));
-    document.getElementById(pagina).classList.add("active");
-    history.pushState({ pagina }, "", `#${pagina}`);
+    document.querySelectorAll("section").forEach(sec => sec.classList.remove("active"))
+    document.getElementById(pagina).classList.add("active")
+    history.pushState({ pagina }, "", `#${pagina}`)
+}
+
+function calculo() {
+    let consumo = parseFloat(document.getElementById("consumo").value)
+    let gerPP = parseFloat(document.getElementById("gerPP").value)
+
+    resultado.innerHTML = ''
+
+    if (consumo > 0 && gerPP > 0) {
+        const placas = Math.ceil(consumo / gerPP)
+        resultado.innerHTML += `Você precisaria de aproximadamente ${placas} placas.`
+    } else {
+        resultado.innerHTML += 'Você deve digitar valores validos'
+    }
 }
